486895019ac1abf2a30a519bdec619bd79910ad1
Marian <Marian@DESKTOP-HPH0MN8>
2017-03-25 17:39:23 +0200

    Another change.

---
+++ Model/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Employee: Entity
+    {
+        private int employeeID;
+        private string name;
+        private string username;
+        private string password;
+        private string isAdmin;
+
+        public Employee(int employeeID, string name, string username, 
+            string password, string isAdmin)
+        {
+            this.employeeID = employeeID;
+            this.name = name;
+            this.username = username;
+            this.password = password;
+            this.isAdmin = isAdmin;
+        }
+
+
+        public int EmployeeID
+        {
+            get { return employeeID; }
+            set {  employeeID = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set {  password = value; }
+        }
+
+        public string IsAdmin
+        {
+            get { return isAdmin; }
+            set { isAdmin = value; }
+        }
+    }
+}
